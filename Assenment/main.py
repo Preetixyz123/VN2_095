20e69d36dabb33ee8937fbec83099c37745d4ee8
preeti <p2mangal@gmail.com>
2022-05-07 12:50:18 +0530

    harshaassessment

---
+++ Assenment/main.py
@@ -0,0 +1,7 @@
+x = {}
+n = int(input("Enter the elements to add : "))
+for i in range(n):
+       k = input('Enter key: ')
+       v = input('Enter value: ')
+       x[k] = v
+print('Resultant output: \n', x)
